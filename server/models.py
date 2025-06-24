f6651fb4268c8f3f6a4ea5a658cbbd33848280de
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-24 14:07:42 +0300

    correct indentation

--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -25,7 +25,7 @@ class Restaurant(db.Model, SerializerMixin):
     pizzas = relationship("Pizza", secondary="restaurant_pizzas", viewonly=True)
 
     # add serialization rules
-     serialize_rules = ("-restaurant_pizzas.restaurant",)
+    serialize_rules = ("-restaurant_pizzas.restaurant",)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -39,11 +39,10 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
-
-      restaurant_pizzas = relationship("RestaurantPizza", back_populates="pizza")
+    restaurant_pizzas = relationship("RestaurantPizza", back_populates="pizza")
 
     # add serialization rules
-     serialize_rules = ("-restaurant_pizzas.pizza",)
+    serialize_rules = ("-restaurant_pizzas.pizza",)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -55,21 +54,22 @@ class RestaurantPizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+
     # add relationships
-     pizza = relationship("Pizza", back_populates="restaurant_pizzas")
+    pizza = relationship("Pizza", back_populates="restaurant_pizzas")
     restaurant = relationship("Restaurant", back_populates="restaurant_pizzas")
 
-
     # add serialization rules
-
-      serialize_rules = ("-pizza.restaurant_pizzas", "-restaurant.restaurant_pizzas")
-
+    serialize_rules = ("-pizza.restaurant_pizzas", "-restaurant.restaurant_pizzas")
 
     # add validation
-    validates("price")
+    @validates("price")
     def validate_price(self, key, value):
         if value < 1 or value > 30:
             raise ValueError("validation errors")
         return value
+
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
